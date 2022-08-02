73fbd5185d5f2958b9e492bc0f867bff604899e2
Ukaypromise <promiseukaimo@gmail.com>
2022-08-02 18:11:55 +0100

    Add UI functionality

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Book Clas
 class Book {
   constructor(title, author, isbn) {
-    title = this.title;
-    author = this.author;
-    isbn = this.isbn;
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
 }
 
@@ -19,6 +19,21 @@ class UI {
             <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
     list.appendChild(row);
+  }
+    //   Delete Book
+    static deleteBook(target) {
+    if (target.classList.contains("delete")) {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+    
+
+  //   Clear Fields
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
   }
 
   static displayBooks() {
@@ -37,9 +52,37 @@ class UI {
     const books = StoredBooks;
     books.forEach((book) => UI.addBookToList(book));
   }
+
+  
 }
 // Store Class
 // Event to Display Books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Events for Adding and Removing books
+const form = document.querySelector("#book-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+  
+  
+  if(title === "" || author === "" || isbn === ""){
+    alert("Please fill in all fields");
+  }
+  else{
+    const book = new Book(title, author, isbn);
+    // Add Book to UI
+    UI.addBookToList(book);
+    // Clear Input Fields
+    UI.clearFields();
+  }
+  
+});
+
+// Events for Removing books
+document.querySelector(".book-list").addEventListener("click", (e) => {
+    UI.deleteBook(e.target);
+    
+})
